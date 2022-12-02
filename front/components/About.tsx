b8d021890b8c6107ac27990c7efce20d1522d039
Lukacs Papp <papplukacs@hotmail.com>
2022-12-02 19:54:16 +0000

    started about section styling

--- front/components/About.tsx
+++ front/components/About.tsx
@@ -4,6 +4,8 @@ type Props = {}
 
 export default function About({ }: Props) {
   return (
-    <div className='h-screen'>About</div>
+    <div className='h-screen flex-col text-center md:text-left relative flex md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      <h3 className='absolute top-24 uppercase tracking-[20px]'>About</h3>
+    </div>
   )
 }
