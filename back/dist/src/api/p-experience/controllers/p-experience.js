f1da81adca61b876cd300b599a3b30dbf1d96337
Lukacs Papp <papplukacs@hotmail.com>
2022-12-04 18:34:26 +0000

    experience relations addedon the back end

--- back/dist/src/api/p-experience/controllers/p-experience.js
+++ back/dist/src/api/p-experience/controllers/p-experience.js
@@ -4,4 +4,14 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::p-experience.p-experience');
+exports.default = strapi_1.factories.createCoreController('api::p-experience.p-experience', ({ strapi }) => ({
+    async findOne(ctx) {
+        const { slug } = ctx.params;
+        const query = {
+            filters: { slug },
+            ...ctx.query,
+        };
+        const post = await strapi.entityService.findMany("api::post.post", query);
+        return this.transformResponse(post[0]);
+    }
+}));
--- back/src/api/p-experience/controllers/p-experience.ts
+++ back/src/api/p-experience/controllers/p-experience.ts
@@ -4,4 +4,17 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::p-experience.p-experience');
+export default factories.createCoreController('api::p-experience.p-experience', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const post = await strapi.entityService.findMany("api::post.post", query);
+
+    return this.transformResponse(post[0]);
+  }
+}));
