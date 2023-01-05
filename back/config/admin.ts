27c057045f8687cbac12594f61614f7cf069deaa
Lukacs J Papp <papplukacs@hotmail.com>
2023-01-05 09:54:25 +0000

    changoing premissions

--- back/config/admin.ts
+++ back/config/admin.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: process.env.ADMIN_JWT_SECRET,
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: process.env.API_TOKEN_SALT,
   },
 });
--- back/dist/config/admin.js
+++ back/dist/config/admin.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
     auth: {
-        secret: env('ADMIN_JWT_SECRET'),
+        secret: process.env.ADMIN_JWT_SECRET,
     },
     apiToken: {
-        salt: env('API_TOKEN_SALT'),
+        salt: process.env.API_TOKEN_SALT,
     },
 });
