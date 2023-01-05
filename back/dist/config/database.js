90a2d02e95b2e719551319436a66739532d4898d
Lukacs J Papp <papplukacs@hotmail.com>
2023-01-05 11:50:20 +0000

    bulding

--- back/dist/config/database.js
+++ back/dist/config/database.js
@@ -1,6 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ env }) => ({
+module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
