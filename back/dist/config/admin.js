0d6858f2617afe88c8dae8370238ec5588ecb049
Lukacs J Papp <papplukacs@hotmail.com>
2023-01-05 11:36:56 +0000

    testing

--- back/dist/config/admin.js
+++ back/dist/config/admin.js
@@ -1,10 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ env }) => ({
+module.exports = ({ env }) => ({
     auth: {
-        secret: process.env.ADMIN_JWT_SECRET,
+        secret: env('ADMIN_JWT_SECRET'),
     },
     apiToken: {
-        salt: process.env.API_TOKEN_SALT,
+        salt: env('API_TOKEN_SALT'),
     },
 });
--- back/dist/config/middlewares.js
+++ back/dist/config/middlewares.js
@@ -1,6 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = [
+module.exports = [
     'strapi::errors',
     {
         name: 'strapi::security',
