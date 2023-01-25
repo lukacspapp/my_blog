c5fc2ca671c257095abc0c9949d83398ba8b11ce
Lukacs J Papp <papplukacs@hotmail.com>
2023-01-25 21:37:06 +0000

    clearing old code

--- front/pages/blog/[slug].tsx
+++ front/pages/blog/[slug].tsx
@@ -1,18 +1,9 @@
-import { fetcher } from "../../lib/api";
+
+export default function Slug() {
 
 
-export default function Slug({ post }) {
 
-  const { title, content, authors, categories, image, createdAt } = post.data.attributes;
-  // const medImg = `http://localhost:1337${image.data[0].attributes.formats.small.url}`
-  // const smlImg = `http://localhost:1337${image.data[0].attributes.formats.thumbnail.url}`
-
-  const author = authors.data[0].attributes.name
-
-  const category = categories.data[0].attributes.name
-
-  const images = image.data.map((img) => img.attributes.url)
-
+  return <h1>sanyi</h1>
 
 
 
@@ -22,18 +13,18 @@ export default function Slug({ post }) {
       <div className="hero min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">{title}</h1>
+            {/* <h1 className="text-5xl font-bold">{title}</h1>
             <p className="py-6 text-lg">{content}</p>
             <p>{createdAt.substring(0, 10)}</p>
             <p>{category}</p>
-            <button className="btn btn-primary">{author}</button>
+            <button className="btn btn-primary">{author}</button> */}
           </div>
         </div>
       </div>
       <section className="overflow-hidden text-gray-700">
         <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
           <div className="flex flex-wrap -m-1 md:-m-2">
-            {images.map((img) => {
+            {/* {images.map((img) => {
               return (
                 <div className="flex flex-wrap p-1 w-1/3">
                   <div className="flex flex-wrap -m-1 md:-m-2">
@@ -45,23 +36,10 @@ export default function Slug({ post }) {
                   </div>
                 </div>
               )
-            })}
+            })} */}
           </div>
         </div>
       </section>
     </>
   )
-}
-
-
-export const getServerSideProps = async ({ params }) => {
-  const { slug } = params;
-
-  const post = await fetcher(`http:///localhost:1337/api/posts/${slug}?populate=*`);
-
-  return {
-    props: {
-      post: post
-    }
-  }
 }
--- front/pages/blog/index.tsx
+++ front/pages/blog/index.tsx
@@ -1,4 +1,3 @@
-import { fetcher } from "../../lib/api";
 import { server } from "../../config";
 import BlogPosts from "../../components/Blog/BlogPosts";
 import { useState } from "react";
@@ -17,37 +16,39 @@ interface ServerSideProps {
 
 function Blog({ ServerSidePosts, ServerSideAuthors }: ServerSideProps) {
 
-  const [posts, _setPosts] = useState(ServerSidePosts);
-  const [authors, _setAuthors] = useState(ServerSideAuthors)
+  return <h1>sanyi</h1>
 
-  const authorOfPost = (posts, authors) => {
-    return posts.map(post => {
-      const author = authors.find(author => author.attributes.post.data.id === post.id)
-      return { ...post, author }
-    })
-  }
+  // const [posts, _setPosts] = useState(ServerSidePosts);
+  // const [authors, _setAuthors] = useState(ServerSideAuthors)
 
-  return (
-    <div className="max-w-7xl mx-auto">
-      <BlogHeader />
-      <Banner />
-      <BlogList posts={posts} />
-      {/* <div className='blog-page'>
-        <section className="bg-white dark:bg-gray-900">
-          <div className="container px-6 py-10 mx-auto">
-            <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">From the blog</h1>
-            <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
-              {authorOfPost(posts, authors).map(post => {
-                return (
-                  <BlogPosts key={post.id} post={post} />
-                )
-              })}
-            </div>
-          </div>
-        </section>
-      </div> */}
-    </div>
-  )
+  // const authorOfPost = (posts, authors) => {
+  //   return posts.map(post => {
+  //     const author = authors.find(author => author.attributes.post.data.id === post.id)
+  //     return { ...post, author }
+  //   })
+  // }
+
+  // return (
+  //   <div className="max-w-7xl mx-auto">
+  //     <BlogHeader />
+  //     <Banner />
+  //     <BlogList posts={posts} />
+  //     <div className='blog-page'>
+  //       <section className="bg-white dark:bg-gray-900">
+  //         <div className="container px-6 py-10 mx-auto">
+  //           <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">From the blog</h1>
+  //           <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
+  //             {authorOfPost(posts, authors).map(post => {
+  //               return (
+  //                 <BlogPosts key={post.id} post={post} />
+  //               )
+  //             })}
+  //           </div>
+  //         </div>
+  //       </section>
+  //     </div>
+  //   </div>
+  // )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -63,20 +64,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     };
   }
 
-  const posts: ServerSidePosts = await fetcher(`${server}/posts?populate=*`);
-
-  const authors: any = await fetcher(`${server}/authors?populate=*`);
-
-  if (!posts) {
-    return {
-      notFound: true,
-    }
-  }
 
   return {
     props: {
-      ServerSidePosts: posts.data,
-      ServerSideAuthors: authors.data
+      session
     }
   }
 }
