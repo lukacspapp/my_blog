300d23e244eadcaf171b8462a3d1fa89ce236c8f
Lukacs J Papp <papplukacs@hotmail.com>
2023-02-13 08:21:09 +0000

    added opacity

--- front/components/Nav.tsx
+++ front/components/Nav.tsx
@@ -8,9 +8,9 @@ export function Nav() {
   return (
     <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50 p-5">
       <div className="container mx-auto">
-        <div className="w-full bg-black/20 items-center h-[96px] backdrop-blur-2xl rounded-full mx-auto max-w-md px-5 flex justify-between text-2xl">
+        <div className="w-full bg-black/5 items-center h-[96px] backdrop-blur-2xl rounded-full mx-auto max-w-md px-5 flex justify-between text-2xl">
           <Link to='hero' className="cursor-pointer w-[40px] h-[40px] flex items-center" >
-            <HomeIcon />
+            <HomeIcon  />
           </Link>
           <Link to='about' className="cursor-pointer w-[40px] h-[40px] flex items-center">
             <UserIcon />
