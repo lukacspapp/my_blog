c633c4afcf38ac5c9fde6b64115d7f5b1870ca1c
Lukacs J Papp <papplukacs@hotmail.com>
2023-02-15 09:13:31 +0000

    addedd css

--- front/components/Portfolio/Skill.tsx
+++ front/components/Portfolio/Skill.tsx
@@ -10,7 +10,7 @@ export default function Skill({ directionLeft, technology }: Props) {
 
 
   return (
-    <div className='m-2 group relative flex cursor-pointer '>
+    <div className='text-center cursor-pointer p-2 w-[80%]'>
       <motion.img
         initial={{
           x: directionLeft ? -200 : 200,
@@ -20,7 +20,7 @@ export default function Skill({ directionLeft, technology }: Props) {
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         src={technology.image.url}
-        className=' bg-gradient-to-r from-gray-700 via-gray-900 to-black rounded-3xl p-4 object-contain h-10 w-10 md:w-28 md:h-28 xl:object-scale-down xl:w-32 xl:h-32
+        className='bg-gradient-to-r from-gray-700 via-gray-900 to-black rounded-3xl p-4 object-contain h-10 w-10 md:w-28 md:h-28 xl:object-scale-down xl:w-32 xl:h-32
         filter grayscale hover:grayscale-0 transition duration-300 ease-in-out'
       />
     </div>
