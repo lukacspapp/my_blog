bb3323c027b16dd6788b06ed15afaae1c0360f34
Lukacs J Papp <papplukacs@hotmail.com>
2023-03-28 17:48:48 +0100

    testing

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,5 +1,3 @@
-import "tailwindcss/tailwind.css";
-import '../styles/index.css';
 import { Providers } from './providers';
 
 export default function RootLayout({
@@ -10,7 +8,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en" className='nightwind h-full'>
       <head />
-      <body className='bg-gray-50 selection:bg-teal-300 selection:text-gray-800 dark:bg-gray-900 dark:selection:bg-rose-600 dark:selection:text-rose-50'>
+      <body className="bg-gray-50 selection:bg-teal-300 selection:text-gray-800 dark:bg-gray-900 dark:selection:bg-rose-600 dark:selection:text-rose-50" >
         <Providers>
           {children}
         </Providers>
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,40 +1,44 @@
 'use client'
 
-import { TooltipProvider } from "@radix-ui/react-tooltip";
-import clsx from "clsx";
-import Inspect from "inspx";
-import Navigation from "../components/Navigation/Navigation";
-import TransitionPage from "../components/TransitionPage";
-import TransitionProvider from "../components/TransitionProvider";
+import { TooltipProvider } from "@radix-ui/react-tooltip"
+import { Analytics } from "@vercel/analytics/react"
+import clsx from "clsx"
+import Inspect from "inspx"
+import "tailwindcss/tailwind.css"
+import NavigationBar from "../components/Navigation/Navigation"
+import TransitionProvider from "../components/TransitionProvider"
 
-const redGradient = "bg-gradient-to-r from-rose-500 via-pink-500 to-purple-500 text-transparent bg-clip-text"
+export default function Page(): JSX.Element {
 
-
-export default function Home() {
 
   return (
-    <Inspect>
-      <div className="mx-auto max-w-screen-lg">
-        <span
-          className={clsx(
-            "rounded-full bg-gradient-to-r",
-            "dark:from-rose-700 dark:via-pink-700 dark:to-purple-700 dark:opacity-60",
-            "from-blue-300 via-cyan-300 to-green-300 opacity-70",
-            "-z-50 aspect-square w-full max-w-screen-lg blur-3xl filter",
-            "bottom-[calc(100%-200px)] dark:bottom-[calc(100%-180px)]",
-            "absolute"
-          )}
-        />
-      </div>
-      <TooltipProvider>
-        <Navigation />
-      </TooltipProvider>
-      <TransitionPage title={'Lukacs Papp'} description={'Hello Sanyi'}>
-        <TransitionProvider>
-          <h1>sanyi</h1>
-        </TransitionProvider>
-      </TransitionPage>
-    </Inspect>
-  );
+        <Inspect>
+          <div className="mx-auto max-w-screen-lg">
+            <span
+              className={clsx(
+                "rounded-full bg-gradient-to-r",
+                "dark:from-rose-700 dark:via-pink-700 dark:to-purple-700 dark:opacity-60",
+                "from-blue-300 via-cyan-300 to-green-300 opacity-70",
+                "-z-50 aspect-square w-full max-w-screen-lg blur-3xl filter",
+                "bottom-[calc(100%-200px)] dark:bottom-[calc(100%-180px)]",
+                "fixed"
+              )}
+            />
+          </div>
+          <TooltipProvider>
+            <NavigationBar />
+          </TooltipProvider>
+          <main className="body">
+            <TooltipProvider>
+              <TransitionProvider>
+                <h1>sanyi</h1>
+              </TransitionProvider>
+            </TooltipProvider>
+          </main>
+          <>
+          </>
+          <Analytics />
+        </Inspect>
+  )
 }
 
