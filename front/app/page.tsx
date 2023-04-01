3b910f4bbf8c09b93d93e8eecec39b5f9141b2c1
Lukacs J Papp <papplukacs@hotmail.com>
2023-04-01 22:43:22 +0100

    prettier

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -10,21 +10,21 @@ export default function Page(): JSX.Element {
   return (
     <>
       <main className="body">
-          <About />
-          <AnimatedDescription title={''} description={''} />
-          <BrowserFrame
-            href="/github?search=lukacspapp"
-            title="Github Contributions"
-            containerClassName="relative m-auto h-full w-full max-w-sm sm:max-w-md grayscale-0 group-hover:grayscale-0 sm:grayscale"
-          >
-            <Image
-              src={"/images/github.svg"}
-              alt={"github-contributions"}
-              width={1920}
-              height={1080}
-              priority={true}
-            />
-          </BrowserFrame>
+        <About />
+        <AnimatedDescription title={''} description={''} />
+        <BrowserFrame
+          href="/github?search=lukacspapp"
+          title="Github Contributions"
+          containerClassName="relative m-auto h-full w-full max-w-sm sm:max-w-md grayscale-0 group-hover:grayscale-0 sm:grayscale"
+        >
+          <Image
+            src={"/images/github.svg"}
+            alt={"github-contributions"}
+            width={1920}
+            height={1080}
+            priority={true}
+          />
+        </BrowserFrame>
       </main>
     </>
   )
