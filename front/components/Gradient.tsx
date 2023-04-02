a99e1abc6a54eba11916d81b82737760b4a5b682
Lukacs J Papp <papplukacs@hotmail.com>
2023-04-02 10:56:24 +0100

    trying the new look

--- front/components/Gradient.tsx
+++ front/components/Gradient.tsx
@@ -1,10 +1,10 @@
 
 export default function Gradient() {
-  const gradientClass = `rounded-full bg-gradient-to-r dark:from-rose-700 dark:via-pink-700 dark:to-purple-700 dark:opacity-60 from-blue-300 via-cyan-300 to-green-300 opacity-70 -z-50 aspect-square w-full max-w-screen-lg blur-3xl filter bottom-[calc(100%-200px)] dark:bottom-[calc(100%-180px)]`;
+  const gradientClasses = `rounded-full bg-gradient-to-r dark:from-rose-700 dark:via-pink-700 dark:to-purple-700 dark:opacity-60 from-blue-300 via-cyan-300 to-green-300 opacity-70 -z-50 aspect-square w-full max-w-screen-md blur-3xl filter`;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center">
-      <div className={gradientClass} />
+    <div className="fixed inset-x-0 top-0 flex justify-center transform -translate-y-3/4">
+      <div className={gradientClasses} />
     </div>
   )
 }
