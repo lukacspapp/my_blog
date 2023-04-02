366bbd5b920022ef055a94c22aa504792e98cb59
Lukacs J Papp <papplukacs@hotmail.com>
2023-04-02 14:23:43 +0100

    changing gradient

--- front/components/Gradient.tsx
+++ front/components/Gradient.tsx
@@ -5,7 +5,7 @@ export default function Gradient() {
   const honeyGradient = `rounded-full bg-gradient-to-r dark:from-rose-700 dark:via-pink-700 dark:to-purple-700 dark:opacity-60 from-blue-300 via-cyan-300 to-green-300 opacity-70 -z-50 aspect-square w-full max-w-screen-md blur-3xl filter`;
 
   return (
-    <div className="fixed inset-x-0 top-0 flex justify-center transform -translate-y-3/4">
+    <div className="fixed inset-x-0 top-0 flex justify-center transform -translate-y-2/3">
       <div className={gradientClasses} />
     </div>
   )
