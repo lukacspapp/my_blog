58efd21a2d9660a3cf2c0072c909290ef3014b6e
Lukacs J Papp <papplukacs@hotmail.com>
2023-04-06 22:22:31 +0100

    journal is fetching

--- front/app/projects/page.tsx
+++ front/app/projects/page.tsx
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import matter from "gray-matter"
 import Description from "../../components/Github/Desciption"
 import TransitionPage from "../../components/TransitionPage"
 
@@ -6,11 +8,23 @@ const description = 'A list of projects I have worked on.'
 
 export default async function Projects() {
 
+  const fileNames = fs.readdirSync("./data/projects")
+  console.log(fileNames)
+
+  const projects = (
+    await Promise.all(fileNames.map(fileName => matter.read((`./data/projects/${fileName}`)).data)))
+    .sort((a, b) => new Date(b.published).getTime() - new Date(a.published).getTime())
+
+    console.log('====================================');
+    console.log(projects);
+    console.log('====================================');
+
 
   return (
     <div>
       <TransitionPage title={title} description={description}>
         <Description title={title} description={description} />
+
       </TransitionPage>
     </div>
   )
