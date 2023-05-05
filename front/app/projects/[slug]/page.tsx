d4eaa19a61d65831085a102b88ff54ded87678d6
Lukacs J Papp <papplukacs@hotmail.com>
2023-05-05 22:16:11 +0100

    added extra data

--- front/app/projects/[slug]/page.tsx
+++ front/app/projects/[slug]/page.tsx
@@ -1,17 +1,28 @@
 import matter from "gray-matter";
 
-export default async function SlugPage({ params }) {
+export default async function SlugPage({ params })  {
 
   const { slug } = params
 
-  const {data, content} = matter.read(`./data/projects/${slug}.mdx`)
+  const readMatterFile = (filePath: string) => {
+    return new Promise((resolve, reject) => {
+      try {
+        const { data, content } = matter.read(filePath);
+        resolve({ data, content });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
 
+  const {data, content} = await readMatterFile(`./data/projects/${slug}.mdx`) as {data: {title: string}, content: string}
 
   return (
     <main className='body'>
       <h1>
         {data.title}
       </h1>
+      <p>{content}</p>
     </main>
   )
 }
