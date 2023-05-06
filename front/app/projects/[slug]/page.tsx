5a086321cb03ec06166082d586f24610c86db923
Lukacs J Papp <papplukacs@hotmail.com>
2023-05-06 11:44:08 +0100

    path is generated

--- front/app/projects/[slug]/page.tsx
+++ front/app/projects/[slug]/page.tsx
@@ -1,11 +1,10 @@
 import matter from "gray-matter";
-import path from "path"; // Add this import
+import path from "path";
 import readingTime from "reading-time";
 
 export default async function SlugPage({ params }) {
   const { slug } = params;
 
-  // Update this part to resolve the file path correctly
   const filePath = path.join(process.cwd(), "data", "projects", `${slug}.mdx`);
   const file = matter.read(filePath);
 
