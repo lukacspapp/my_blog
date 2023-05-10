ef67809c64c5afa880a8a20e55febb095942b244
Lukacs J Papp <papplukacs@hotmail.com>
2023-05-10 14:15:34 +0100

    added type

--- front/types/portfolioTypes.ts
+++ front/types/portfolioTypes.ts
@@ -1,12 +1,3 @@
-export interface Experience {
-  companyTitle: string,
-  endDate: string,
-  id: string,
-  jobTitle: string,
-  portfolioTechnologies: Technology[],
-  startDate: string,
-  image: Image
-}
 export interface ProjectsData {
   projects: Projects[]
 }
@@ -32,48 +23,6 @@ export interface Project {
 export interface ProjectContent {
   markdown: string
 }
-
-
-export interface Technology {
-  Image: Image,
-  name: string
-}
-
-export interface Hero {
-  id: string,
-  profile: Image,
-  vocations: string
-}
-
-export interface About {
-  id: string,
-  photo: Image,
-  bio: string,
-  location: string,
-  title: string,
-}
-
-export interface Image {
-  url: string,
-  id: string
-}
-
-export interface PortfolioTechnologies {
-  portfolioTechnologies: Technology[]
-}
-
-export interface PortfolioExpriences {
-  portfolioExpriences: Experience[]
-}
-
-export interface PortfolioHero {
-  portfolioHeroes: Hero[]
-}
-
-export interface PortfolioAbout {
-  potfolioAbouts: About[]
-}
-
 export interface Description  {
   title: string
   description: string
