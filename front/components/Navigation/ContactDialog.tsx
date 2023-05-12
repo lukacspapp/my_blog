cad165328f65c2b210f3bc6f0388aa1f75041ec8
Lukacs J Papp <papplukacs@hotmail.com>
2023-05-12 17:09:40 +0100

    moredesign

--- front/components/Navigation/ContactDialog.tsx
+++ front/components/Navigation/ContactDialog.tsx
@@ -7,17 +7,17 @@ const DialogDemo = ({ children }) => (
       {children}
     </Dialog.Trigger>
     <Dialog.Portal>
-      <Dialog.Overlay className="bg-[#101010] opacity-70 fixed inset-0" />
+      <Dialog.Overlay className="bg-[#0a0a0a] opacity-70 fixed inset-0" />
       <div className="animate-slide-in fixed top-[50%] left-[50%]">
         <Dialog.Content
-          className="animate-dialog-slide-in flex flex-col justify-between items-stretch z-10 fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[8px] bg-[#202020] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
+          className="animate-dialog-slide-in border-[#272727] border flex flex-col justify-between items-stretch z-10 fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[8px] bg-[#0a0a0a] p-[10px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
         >
-          <Dialog.Title className="text-gray-600 dark:text-gray-400 m-0 text-lg flex-row justify-between p-4 font-medium">
+          <Dialog.Title className="text-gray-600 dark:text-gray-400 text-lg flex-row justify-between p-4 font-medium">
             Contact
           </Dialog.Title>
-          <Dialog.Description className="text-gray-600 p-4 dark:text-gray-400 mt-2 mb-4 items-center">
+          <Dialog.Description className="text-gray-600 h-40 p-5 dark:text-gray-400 mt-2 mb-4 items-center">
             <div
-              className="flex flex-row justify-between pl-4 items-center"
+              className="flex flex-row justify-between items-center w-2"
             >
               <div
                 className="flex flex-col w-52"
@@ -29,29 +29,46 @@ const DialogDemo = ({ children }) => (
                   papplukacs@hotmail.com
                 </p>
               </div>
-              <div
-                className="flex flex-row rounded-md border-2 border-gray-500 dark:border-[#282828]"
-              >
-                <a
-                  href="mailto:papplukacs@hotmail.com"
-                  className=" border-r-2 flex dark:border-[#282828] pl-3 pt-2 pr-2 pb-2"
-                  rel="noreferrer noopener"
-                  target="_blank"
+                <div
+                  className="flex flex-col w-auto m-3"
                 >
-                  <ClipboardIcon className="h-5 w-5 text-gray-500" />
-                  Compose
-                </a>
-                <button
-                  className="flex pl-3 pt-2 pr-2 pb-2"
-                  onClick={() => {
-                    navigator.clipboard.writeText("papplukacs@hotmail.com");
-                  }}
-                >
-                  <DocumentDuplicateIcon className="h-5 w-5 text-gray-500" />
-                  Copy
-                </button>
+                  <div
+                    className="border-2 mb-2 rounded-md border-gray-500 dark:border-[#282828]"
+                  >
+                    <a
+                      href="mailto:papplukacs@hotmail.com"
+                      className="  flex dark:border-[#282828] pl-3 pt-2 pr-2 pb-2"
+                      rel="noreferrer noopener"
+                      target="_blank"
+                    >
+                      <ClipboardIcon className="h-5 w-5 text-gray-500" />
+                      <p
+                        className="text-sm text-gray-500"
+                      >
+                        Compose
+                      </p>
+                    </a>
+                  </div>
+                  <div
+                    className="border-2 rounded-md border-gray-500 dark:border-[#282828]"
+                  >
+                    <button
+                      className="flex pl-3 p-2 dark:border-[#282828]"
+                      onClick={() => {
+                        navigator.clipboard.writeText("papplukacs@hotmail.com");
+                      }}
+                    >
+                      <DocumentDuplicateIcon className="h-5 w-5 text-gray-500" />
+                      <p
+                        className="text-sm ml-2 text-gray-500"
+                      >
+                        Copy
+                      </p>
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
+
           </Dialog.Description>
         </Dialog.Content>
       </div>
