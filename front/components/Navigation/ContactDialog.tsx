45cee8885f24aa0835dfbc26a06f6f932abc2cc2
Lukacs J Papp <papplukacs@hotmail.com>
2023-05-12 18:07:37 +0100

    dark theme is done

--- front/components/Navigation/ContactDialog.tsx
+++ front/components/Navigation/ContactDialog.tsx
@@ -10,12 +10,12 @@ const DialogDemo = ({ children }) => (
       <Dialog.Overlay className="bg-[#0a0a0a] opacity-70 fixed inset-0" />
       <div className="animate-slide-in fixed top-[50%] left-[50%]">
         <Dialog.Content
-          className="animate-dialog-slide-in border-[#272727] border flex flex-col justify-between items-stretch z-10 fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[8px] bg-[#0a0a0a] p-[10px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
+          className="animate-dialog-slide-in border-[#272727] border flex flex-col justify-between items-stretch z-10 fixed top-[50%] left-[50%] max-h-[85vh] w-[80vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[8px] bg-[#0a0a0a] p-[10px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
         >
-          <Dialog.Title className="text-gray-600 dark:text-gray-400 text-lg flex-row justify-between p-4 font-medium">
+          <Dialog.Title className="text-gray-400 dark:text-gray-600 text-lg flex-row justify-between p-4 font-medium">
             Contact
           </Dialog.Title>
-          <Dialog.Description className="text-gray-600 p-5 dark:text-gray-400 mt-2 mb-4 items-center">
+          <Dialog.Description className="text-gray-400 p-5 dark:text-gray-600 mt-2 mb-4 items-center">
             <div
               className="flex flex-row justify-between items-center space-x-6 md:space-x-0 md:grid md:grid-cols-2"
             >
@@ -25,7 +25,7 @@ const DialogDemo = ({ children }) => (
                 <p className='mb-1'>
                   Email
                 </p>
-                <p className='text-sm text-gray-500'>
+                <p className='text-sm text-gray-500 dark:text-gray-400'>
                   papplukacs@hotmail.com
                 </p>
               </div>
@@ -41,7 +41,7 @@ const DialogDemo = ({ children }) => (
                     rel="noreferrer noopener"
                     target="_blank"
                   >
-                    <PencilSquareIcon className="h-5 w-5 text-gray-500" />
+                    <PencilSquareIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
                   </a>
                 </div>
                 <div
@@ -53,7 +53,7 @@ const DialogDemo = ({ children }) => (
                       navigator.clipboard.writeText("papplukacs@hotmail.com");
                     }}
                   >
-                    <DocumentDuplicateIcon className="h-5 w-5 text-gray-500" />
+                    <DocumentDuplicateIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
                   </button>
                 </div>
               </div>
