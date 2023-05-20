259ec0601bb01ed0ab98345fbadec271715a25dd
Lukacs J Papp <papplukacs@hotmail.com>
2023-05-20 11:00:42 +0100

    formatting

--- front/app/error.tsx
+++ front/app/error.tsx
@@ -53,19 +53,19 @@ export default function Error({ error, reset }: ErrorProps) {
         </p>
         <br />
         <div className='flex flex-row justify-center space-x-2'>
-        <div
-                    className="border-2 mb-2 rounded-md border-gray-500 dark:border-gray-600"
-                  >
-                    <button
-                      onClick={reset}
-                      className="flex cursor-pointer dark:border-[#282828] rounded-[3px] pl-3 pt-2 pr-2 pb-2 transition-colors duration-300 ease-in-out hover:bg-gray-300 dark:hover:bg-gray-600"
-                    >
-                      <p className='mr-1 text-md text-black dark:text-white'>
-                        Refresh
-                      </p>
-                      <RefreshCcw className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-                    </button>
-                  </div>
+          <div
+            className="border-2 mb-2 rounded-md border-gray-500 dark:border-gray-600"
+          >
+            <button
+              onClick={reset}
+              className="flex cursor-pointer dark:border-[#282828] rounded-[3px] pl-3 pt-2 pr-2 pb-2 transition-colors duration-300 ease-in-out hover:bg-gray-300 dark:hover:bg-gray-600"
+            >
+              <p className='mr-1 text-md text-black dark:text-white'>
+                Refresh
+              </p>
+              <RefreshCcw className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+            </button>
+          </div>
         </div>
       </Transition>
     </div>
