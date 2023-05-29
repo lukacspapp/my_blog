dcff94030463d0d5e37964eae46a7b5c29a63379
Lukacs J Papp <papplukacs@hotmail.com>
2023-05-29 14:08:21 +0200

    images are selected;

--- front/components/JournalLayout.tsx
+++ front/components/JournalLayout.tsx
@@ -1,11 +1,32 @@
-import Head from "next/head"
-import ReactHtmlParser from "react-html-parser"
+'use client'
+
+import Head from "next/head";
+import ReactHtmlParser from "react-html-parser";
 
 type JournalLayoutProps = {
   content: string
 }
 
 export default function JournalLayout({ content }: JournalLayoutProps) {
+
+  console.log(content);
+
+
+  function wrapImagesWithFramerMotion(html) {
+    const images = html.match(/<img[^>]+>/g);
+
+    if (images) {
+      images.forEach((img) => {
+        const src = img.match(/src="([^"]*)/)[1];
+      });
+      return html;
+    }
+  }
+
+  wrapImagesWithFramerMotion(content);
+
+
+
   return (
     <article className="prose prose-neutral prose-headings:font-medium prose-a:no-underline prose-pre:rounded-lg prose-ul:my-5 prose-img:rounded-xl dark:prose-invert">
       <Head>
