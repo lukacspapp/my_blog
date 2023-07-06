36bdd3a3450283e2fb98ff7b9b9d03cac321df71
Lukacs J Papp <papplukacs@hotmail.com>
2023-07-06 09:27:36 +0100

    added next-auth

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -3,16 +3,18 @@ import "tailwindcss/tailwind.css"
 import About from "../components/About"
 import { getBio } from "../lib/services"
 import Chat from "../components/Chat/Chat"
+import { getServerSession } from "next-auth"
 
 export default async function Page() {
 
+  const session = await getServerSession()
   const bio = await getBio()
 
   return (
     <>
       <main className="body">
         <About bio={bio}/>
-        {/* <Chat /> */}
+        <Chat />
       </main>
     </>
   )
