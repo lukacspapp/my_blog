4f8591b3007bc142d1b6532847eb07d5c02f0bea
Lukacs J Papp <papplukacs@hotmail.com>
2023-07-10 00:51:18 +0100

    icons are done

--- front/components/Auth/LoginDialog.tsx
+++ front/components/Auth/LoginDialog.tsx
@@ -22,7 +22,7 @@ export default function LoginDialog({ children }) {
                   <button
                     aria-label="Continue with google"
                     role="button"
-                    className=" py-2.5 px-3 flex mb-2 justify-center rounded-md transition-colors duration-300 ease-in-out hover:bg-gray-300 dark:hover:bg-gray-600 border border-solid border-gray-500 dark:border-gray-600"
+                    className=" items-center py-2.5 px-3 flex mb-2 justify-center rounded-md transition-colors duration-300 ease-in-out hover:bg-gray-300 dark:hover:bg-gray-600 border border-solid border-gray-500 dark:border-gray-600"
                     onClick={() => signIn('google')}
                   >
                     <svg
@@ -31,10 +31,13 @@ export default function LoginDialog({ children }) {
                       viewBox="0 0 19 20"
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4s"
                     >
                       <path
                         d="M18.9892 10.1871C18.9892 9.36767 18.9246 8.76973 18.7847 8.14966H9.68848V11.848H15.0277C14.9201 12.767 14.3388 14.1512 13.047 15.0812L13.0289 15.205L15.905 17.4969L16.1042 17.5173C17.9342 15.7789 18.9892 13.221 18.9892 10.1871Z"
                         fill="#4285F4"
+                        fillRule='evenodd'
+                        clipRule={'evenodd'}
                       />
                       <path
                         d="M9.68813 19.9314C12.3039 19.9314 14.4999 19.0455 16.1039 17.5174L13.0467 15.0813C12.2286 15.6682 11.1306 16.0779 9.68813 16.0779C7.12612 16.0779 4.95165 14.3395 4.17651 11.9366L4.06289 11.9465L1.07231 14.3273L1.0332 14.4391C2.62638 17.6946 5.89889 19.9314 9.68813 19.9314Z"
@@ -49,25 +52,41 @@ export default function LoginDialog({ children }) {
                         fill="#EB4335"
                       />
                     </svg>
-                    <p className="font-medium ml-4 text-black dark:text-white">Continue with Google</p>
                   </button>
                   <button
                     aria-label="Continue with Facebook"
                     role="button"
-                    className="py-2.5 px-3 transition-colors justify-center  duration-300 ease-in-out hover:bg-gray-300 dark:hover:bg-gray-600 border rounded-md border-solid border-gray-500 dark:border-gray-600 flex items-center"
+                    className="py-2.5 px-5 mb-2 transition-colors justify-center  duration-300 ease-in-out hover:bg-gray-300 dark:hover:bg-gray-600 border rounded-md border-solid border-gray-500 dark:border-gray-600 flex items-center"
                     onClick={() => signIn('facebook')}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                      width={19}
-                      height={20}
-                      viewBox="0 0 1365.333 1365.333"
-                    >
-                        <path
-                          d="M1365.333 682.667C1365.333 305.64 1059.693 0 682.667 0 305.64 0 0 305.64 0 682.667c0 340.738 249.641 623.16 576 674.373V880H402.667V682.667H576v-150.4c0-171.094 101.917-265.6 257.853-265.6 74.69 0 152.814 13.333 152.814 13.333v168h-86.083c-84.804 0-111.25 52.623-111.25 106.61v128.057h189.333L948.4 880H789.333v477.04c326.359-51.213 576-333.635 576-674.373" fill="#1877f2"/><path d="M948.4 880l30.267-197.333H789.333V554.609C789.333 500.623 815.78 448 900.584 448h86.083V280s-78.124-13.333-152.814-13.333c-155.936 0-257.853 94.506-257.853 265.6v150.4H402.667V880H576v477.04a687.805 687.805 0 00106.667 8.293c36.288 0 71.91-2.84 106.666-8.293V880H948.4"
-                          fill="#fff"
-                        />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 50 50"
+                    width={19} height={20}
+                    className="h-5 w-5"
+                  >
+
+                    <path
+                      fillRule='evenodd'
+                      clipRule='evenodd'
+                      d="M25,3C12.85,3,3,12.85,3,25c0,11.03,8.125,20.137,18.712,21.728V30.831h-5.443v-5.783h5.443v-3.848 c0-6.371,3.104-9.168,8.399-9.168c2.536,0,3.877,0.188,4.512,0.274v5.048h-3.612c-2.248,0-3.033,2.131-3.033,4.533v3.161h6.588 l-0.894,5.783h-5.694v15.944C38.716,45.318,47,36.137,47,25C47,12.85,37.15,3,25,3z"
+                      fill="#1877f2"
+                    />
                     </svg>
-                    <p className="font-medium ml-4 text-black dark:text-white">Continue with Facebook</p>
+                  </button>
+                  <button
+                    aria-label="Continue with GitHub"
+                    role="button"
+                    className="py-2.5 px-3 transition-colors justify-center duration-300 ease-in-out hover:bg-gray-300 dark:hover:bg-gray-600 border rounded-md border-solid border-gray-500 dark:border-gray-600 flex items-center"
+                    onClick={() => signIn('github')}
+                  >
+                    <svg className='w-5 h-5' viewBox="0 0 24 24" fill="currentColor">
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"
+                      />
+                    </svg>
                   </button>
                 </div>
               </Dialog.Description>
