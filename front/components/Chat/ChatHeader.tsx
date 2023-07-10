4722618a18fade4f419d501d2f416c308f0eec86
Lukacs J Papp <papplukacs@hotmail.com>
2023-07-10 01:03:13 +0100

    testing

--- front/components/Chat/ChatHeader.tsx
+++ front/components/Chat/ChatHeader.tsx
@@ -1,11 +1,20 @@
 'use client'
 
+import { signOut } from "next-auth/react"
+
 export default function ChatHeader() {
   return (
     <div className='w-full flex gap-3 justify-start items-center dark:text-white'>
       <div className='flex flex-col text-sm items-start'>
+        <button
+          onClick={() => {
+            signOut()
+          }}
+        >
+          Sign Out
+        </button>
         <p className="text-sm">
-          Chat With
+          Chat with
         </p>
         <div className="flex gap-1.5 items-center">
           <p className="w-2 h-2 rounded-full bg-green-500 animate-pulse"/>
