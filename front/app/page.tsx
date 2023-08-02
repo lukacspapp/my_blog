558c354eb1bf194439882a8857771d4229688a8b
Lukacs J Papp <papplukacs@hotmail.com>
2023-08-02 08:42:50 +0100

    supabase auth is working

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -3,10 +3,12 @@ import "tailwindcss/tailwind.css"
 import About from "../components/About"
 import { getBio } from "../lib/services"
 import Chat from "../components/Chat/Chat"
+import Loginer from "../components/Auth/Loginer"
 
 export default async function Page() {
 
   const bio = await getBio()
+
 
   return (
     <>
